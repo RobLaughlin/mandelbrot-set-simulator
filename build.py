34e11b74ebc435cc02921c8fb2c6ccbe5ea7277a
RobLaughlin <waterfallkicks@gmail.com>
2020-09-27 09:50:33 -0700

    Update build.py

--- build.py
+++ build.py
@@ -5,13 +5,16 @@ from shutil import copyfile
 if not os.path.exists('build') or not os.path.isdir('build'):
     os.mkdir('build')
 
-copy_tree('img', 'build/img')
-copy_tree('Modules', 'build/Modules')
-copy_tree('AppConfig', 'build/Appconfig')
-copyfile('config.json', 'build/config.json')
+try:
+    copy_tree('img', 'build/img')
+    copy_tree('Modules', 'build/Modules')
+    copyfile('config.json', 'build/config.json')
 
-os.system('pyinstaller --onefile main.py')
-copyfile('dist/main.exe', 'build/main.exe')
-remove_tree('dist')
-remove_tree('build/main')
-os.remove('main.spec')
+    os.system('pyinstaller --onefile app.py')
+
+    copyfile('dist/app.exe', 'build/app.exe')
+    remove_tree('dist')
+    remove_tree('build/app')
+    os.remove('app.spec')
+except:
+    pass
